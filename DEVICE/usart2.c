6d90696e980ede4a5f6fd2230e59138fa37ae7e3
Bashpow <b31296637828@126.com>
2021-11-01 09:56:46 +0800

    加入串口2

---
+++ DEVICE/usart2.c
@@ -0,0 +1,58 @@
+#include "usart2.h"
+
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB
+#if 1
+#pragma import(__use_no_semihosting)
+//标准库需要的支持函数
+struct __FILE
+{ 
+	int handle; 
+}; 
+
+FILE __stdout;
+//定义_sys_exit()以避免使用半主机模式    
+void _sys_exit(int x) 
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{ 	
+	while((USART2->ISR & (uint32_t)0x80) != 0x80){}//循环发送,直到发送完毕
+	USART2->TDR = (uint8_t) ch;      
+	return ch;
+}
+#endif
+
+void Usart2_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure = {0};
+	USART_InitTypeDef USART_InitStructure = {0};
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	/**USART2 GPIO Configuration
+	PA2   ------> USART2_TX
+	PA3   ------> USART2_RX
+	*/
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_1);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_3;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+
+	USART_Cmd(USART2, ENABLE);
+}
---
+++ DEVICE/usart2.h
@@ -0,0 +1,9 @@
+#ifndef __USART2_H_
+#define __USART2_H_
+
+#include "stm32f0xx.h"
+#include "stdio.h"
+
+void Usart2_Init(void);
+
+#endif // __USART2_H_
