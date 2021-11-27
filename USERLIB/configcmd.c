b674ced21867e47b1723f89bc4de0e2fbe8b5cec
Bashpow <B31296637828@hotmail.com>
2021-11-27 15:47:20 +0800

    写了一点串口解析

--- USERLIB/configcmd.c
+++ USERLIB/configcmd.c
@@ -1,6 +1,56 @@
 #include "configcmd.h"
 
+uint8_t Config_Cmd_Len_Check(uint8_t len)
+{
+	if (len == CONFIG_CMD_LENGTH)
+	{
+		return 1;
+	}
+	return 0;
+}
 
+uint8_t Config_Cmd_Rx_Format_Check(uint8_t *arg)
+{
+	// 帧头校验
+	if ((arg[0] & 0xFE) != 0xB4)
+	{
+		return 0;
+	}
 
+	// 帧尾校验
+	if ((arg[6] & 0x0F) != 0x09)
+	{
+		return 0;
+	}
 
+	return 1;
+}
 
+uint8_t Config_Cmd_Crc4_Itu_Check(uint8_t *arg)
+{
+	return 1;
+}
+
+uint8_t Config_Cmd_Cmd_Check(uint8_t *arg)
+{
+	return 1;
+}
+
+uint8_t Config_Cmd_Get_Control_Bit(uint8_t *arg)
+{
+	return (arg[1] & 0x03);
+}
+
+uint8_t Config_Cmd_Implement(uint8_t *arg)
+{
+	switch (arg[1])
+	{
+	case 0x00:
+		return 1;
+	case 0x01:
+		return 1;
+	case 0x02:
+		return 1;
+	}
+	return 0;
+}
--- USERLIB/configcmd.h
+++ USERLIB/configcmd.h
@@ -3,5 +3,9 @@
 
 #include "stm32f0xx.h"
 
+#define CONFIG_CMD_LENGTH 7u
+
+
+
 #endif //_CONFIGCMD_H_
 
