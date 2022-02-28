7d9f35833f700fb9166e38a38c68df637e704225
weihaoysgs <weihao_ysgs@163.com>
2022-02-28 18:28:06 +0800

    完成基本框架

--- DEVICE/can1.h
+++ DEVICE/can1.h
@@ -1,15 +1,25 @@
-#ifndef DEVICE_CAN_H_
-#define DEVICE_CAN_H_
+#ifndef DEVICE_CAN_ONE_H_
+#define DEVICE_CAN_ONE_H_
 #include "can.h"
 
-
-
+namespace device{
+namespace can{
 
 class CanOne : public Can{
 
+public:
+    explicit CanOne(const uint32_t can_id,device::can::Can::CanInitParams& can_init_params);
+    bool CanInit() override;
+    bool CanSendFourMessage() override;
+private:
 
 
 };
 
+}
+}
 
-#endif // DEVICE_CAN_H_
+
+
+
+#endif // DEVICE_CAN_ONE_H_
