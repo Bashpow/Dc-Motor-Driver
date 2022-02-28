eb7f4d0ae9344e8fab3bda9e9a4bd1d1871aee7b
weihaoysgs <weihao_ysgs@163.com>
2022-02-28 14:58:55 +0800

    完成 RTT 移植，以及开启V6编译，去掉printf支持

--- DEVICE/usart2.c
+++ DEVICE/usart2.c
@@ -1,30 +1,30 @@
 #include "usart2.h"
 
 //加入以下代码,支持printf函数,而不需要选择use MicroLIB
-#if 1
-#pragma import(__use_no_semihosting)
-//标准库需要的支持函数
-struct __FILE
-{
-	int handle;
-};
+//#if 1
+//#pragma import(__use_no_semihosting)
+////标准库需要的支持函数
+//struct __FILE
+//{
+//	int handle;
+//};
 
-FILE __stdout;
-//定义_sys_exit()以避免使用半主机模式
-void _sys_exit(int x)
-{
-	x = x;
-}
-//重定义fputc函数
-int fputc(int ch, FILE *f)
-{
-	while ((USART2->ISR & (uint32_t)0x80) != 0x80)
-	{
-	} //循环发送,直到发送完毕
-	USART2->TDR = (uint8_t)ch;
-	return ch;
-}
-#endif
+//FILE __stdout;
+////定义_sys_exit()以避免使用半主机模式
+//void _sys_exit(int x)
+//{
+//	x = x;
+//}
+////重定义fputc函数
+//int fputc(int ch, FILE *f)
+//{
+//	while ((USART2->ISR & (uint32_t)0x80) != 0x80)
+//	{
+//	} //循环发送,直到发送完毕
+//	USART2->TDR = (uint8_t)ch;
+//	return ch;
+//}
+//#endif
 static const uint16_t usart2_dma_rx_max_len = 14;
 static char usart2_rx_buf[usart2_dma_rx_max_len];
 static uint8_t usart2_rxd_length = 0;
