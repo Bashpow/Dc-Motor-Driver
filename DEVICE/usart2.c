b6798c8cf12aea086d3b5bfe094ae791327cc7e3
Bashpow <B31296637828@hotmail.com>
2022-07-04 17:44:52 +0800

    加入文件usart2

---
+++ DEVICE/usart2.c
@@ -0,0 +1,60 @@
+#include "usart2.h"
+
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+
+};
+
+FILE __stdout;
+//定义_sys_exit()以避免使用半主机模式    
+void _sys_exit(int x) 
+{
+	x = x;
+}
+//重定义fputc函数
+int fputc(int ch, FILE *f)
+{
+	while((USART2->SR&0X40)==0){};//循环发送,直到发送完毕
+	USART2->DR = (u8) ch;
+	return ch;
+}
+#endif
+
+//Usart2    PA2--TX    PA3--RX
+void Usart2_Init(u32 bound)
+{
+	//GPIO端口设置
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	//使能USART1，GPIOA时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	//USART2_TX   GPIOA.2
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; //PA.2
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
+	GPIO_Init(GPIOA, &GPIO_InitStructure);//初始化GPIOA.2
+
+	//USART2_RX	  GPIOA.3初始化
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;//PA3
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
+	GPIO_Init(GPIOA, &GPIO_InitStructure);//初始化GPIOA.3
+  
+	//USART 初始化设置
+	USART_InitStructure.USART_BaudRate = bound;//串口波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;	//收发模式
+	USART_Init(USART2, &USART_InitStructure); //初始化串口1
+	
+	USART_Cmd(USART2, ENABLE);                    //使能串口1 
+}
+
---
+++ DEVICE/usart2.h
@@ -0,0 +1,9 @@
+#ifndef __USART2_H
+#define __USART2_H
+
+#include "stm32f10x.h"
+#include "stdio.h"
+
+void Usart2_Init(u32 bound);
+
+#endif
